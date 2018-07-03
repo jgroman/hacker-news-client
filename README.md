b9dbf4c7b6025539ce0a24e89a9c7a75fe7f1682
jgroman <jgroman@users.noreply.github.com>
2018-07-03 21:11:32 +0200

    Initial empty app commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hacker News Client
+
+Udacity Advanced Android Nanodegree project Capstone Part 2
+
+## External libraries used by this project:
+OkHttp - http://square.github.io/okhttp/  
+
+Stetho - http://facebook.github.io/stetho/
---
+++ app/src/main/java/cz/jtek/hackernewsclient/ui/StoriesListActivity.java
@@ -0,0 +1,15 @@
+package cz.jtek.hackernewsclient.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.jtek.hackernewsclient.R;
+
+public class StoriesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stories_list);
+    }
+}
