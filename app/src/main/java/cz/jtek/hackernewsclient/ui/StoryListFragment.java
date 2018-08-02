4bc5f9e0160f82e7cd1370ea12fc1f743e047fc4
jgroman <jgroman@users.noreply.github.com>
2018-08-02 15:33:14 +0200

    wip: StoryActivity

--- app/src/main/java/cz/jtek/hackernewsclient/ui/StoryListFragment.java
+++ app/src/main/java/cz/jtek/hackernewsclient/ui/StoryListFragment.java
@@ -18,15 +18,10 @@ package cz.jtek.hackernewsclient.ui;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
-import android.support.v4.content.AsyncTaskLoader;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -34,18 +29,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
-
 import cz.jtek.hackernewsclient.R;
-import cz.jtek.hackernewsclient.model.HackerNewsApi;
-import cz.jtek.hackernewsclient.model.Item;
-import cz.jtek.hackernewsclient.utils.MockDataUtils;
-import cz.jtek.hackernewsclient.utils.NetworkUtils;
-import cz.jtek.hackernewsclient.utils.NetworkUtils.AsyncTaskResult;
 
 public class StoryListFragment extends Fragment
     implements StoryListAdapter.StoryListOnClickListener {
---
+++ app/src/main/java/cz/jtek/hackernewsclient/ui/StoryWebViewFragment.java
@@ -0,0 +1,6 @@
+package cz.jtek.hackernewsclient.ui;
+
+import android.support.v4.app.Fragment;
+
+public class StoryWebViewFragment extends Fragment {
+}
